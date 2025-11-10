852d93d41ca77dc1aecfbcfffa150aa5bd8fc669
Mern2503 <mdtuhinurrahman2378@gmail.com>
2025-11-10 20:54:24 +0600

    changes on practice for first javascript at html js file

---
+++ script.js
@@ -0,0 +1,39 @@
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+let a = 10;
+a = 20;
+console.log(window);
+
+// const b = 10;
+// b = 30;
+// console.log(b);
+// error
+
+let string = `Tuhinur Rahman`;
+let number = 25;
+let bigInt = 25n;
+let boolean = true;
+let symbol = Symbol(10);
+let undefi;
+let nu = null;
+
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof bigInt);
+console.log(typeof boolean);
+console.log(typeof symbol);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(string,number,bigInt,boolean,symbol,undefi,nu);
+
+let names = ["Mr x","Mr y","Mr z",500,true,null,[]];
+console.log(names);
+console.log(typeof names);
+
+let parson = {name: "Mr x", age: 20, address: "Dhaka"};
+console.log(parson);
+console.log(typeof parson);
+
+
